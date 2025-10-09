65c03264f177b3d73c9a2fd89f098471b726a85f
Carlos Castillo <castillo.st@gmail.com>
2025-10-09 19:42:35 +0200

    Add initial exercise files and scripts for bash exercises

---
+++ 00-fundamentos-linux/shellscripts/ejercicio3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p foo/{empty,dummy}
+echo 'Me encanta la bash!!' > foo/dummy/file1.txt
+touch foo/dummy/file2.txt
+cat ./foo/dummy/file1.txt > ./foo/dummy/file2.txt & mv ./foo/dummy/file2.txt ./foo/empty
