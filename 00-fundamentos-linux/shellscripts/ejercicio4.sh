a9ccfafd6c0136023397a8406202df130e1f237c
Carlos Castillo <castillo.st@gmail.com>
2025-10-11 12:26:03 +0200

    Refactor ejercicio4.sh to accept dynamic URL input and improve error handling

--- 00-fundamentos-linux/shellscripts/ejercicio4.sh
+++ 00-fundamentos-linux/shellscripts/ejercicio4.sh
@@ -7,12 +7,23 @@ set -xue
 # Página sin patatas: https://www.univision.com/estilo-de-vida/asi-se-vive-mejor-hogar/como-barrer-el-piso-sin-dejar-mugre-revelamos-trucos-y-tecnicas-que-si-funcionan
 # ./ejercicio4.sh https://www.univision.com/estilo-de-vida/asi-se-vive-mejor-hogar/como-barrer-el-piso-sin-dejar-mugre-revelamos-trucos-y-tecnicas-que-si-funcionan
 
-HAY_PATATAS=$(curl -s $1 | grep -io "patatas" | wc -l)
+if [[ $# -lt 1 ]]; then
+  echo "Error: es necesario especificar el término a buscar"> /dev/stderr
+  exit 2
+fi
 
-if (($HAY_PATATAS>0))
+if [[ $# -gt 1 ]]; then
+  echo "Warn: sólo se acepta un término de busca, ignorando los demás"
+fi
+
+PAGINA='https://www.cocopot.es/blog/como-se-cultiva/guia-del-cultivo-de-la-patata?srsltid=AfmBOorJQ9bm2225jQi_Z-RHa9elntxmn528WqFv9LoQPNVYeue1chyP'
+
+OCURRENCIAS=$(curl -s $PAGINA | grep -io $1 | wc -l)
+
+if (($OCURRENCIAS>0))
 then
-  echo "tiene patatas"
+  echo "Contiene \"$1\""
   exit 0
 fi
 
-echo "No tiene patatas"
+echo "No contiene \"$1\""
